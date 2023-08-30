b900f75bf1fe56706df71203b601f240f2071a62
FreeNugget <mathieujanisset@hotmail.fr>
2023-08-30 10:40:10 +0200

    all T

---
+++ Python/terre07.py
@@ -0,0 +1,18 @@
+# Size string
+        
+# Time = 0 h
+# Date = 30 aout 2023
+
+import sys
+
+if len(sys.argv) == 2 :
+
+    if isinstance(sys.argv[1], str) == True :
+            
+        In       = list(sys.argv[1])
+        
+        
+        
+        
+        print(In)
+        
